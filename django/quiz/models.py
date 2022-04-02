dd336f92ffcb47172af48efecd89b0038c1638c0
Marcel Schmitt <schmitt.marcel97@gmail.com>
2022-04-02 10:32:05 +0200

    create new models

--- django/quiz/models.py
+++ django/quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Kunyomi(models.Model):
+    kunyomi = models.CharField(max_length=50)
+
+class Onyomi(models.Model):
+    onyomi = models.CharField(max_length=50)
+
+class Meaning(models.Model):
+    meaning = models.CharField(max_length=255)
+
+class Kanji(models.Model):
+    symbol = models.CharField(max_length=10)
+    jlpt = models.IntegerField()
+    kunyomis = models.ManyToManyField(Kunyomi, related_name='kanji_kunyomi')
+    onyomis = models.ManyToManyField(Onyomi, related_name='kanji_onyomi')
+    meanings = models.ManyToManyField(Meaning, related_name='kanji_meaning')
