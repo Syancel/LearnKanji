75e07d5af676c540d1eb95ab6a04edeeb6528c13
Marcel Schmitt <schmitt.marcel97@gmail.com>
2022-04-02 11:08:58 +0200

    change table names

--- django/quiz/models.py
+++ django/quiz/models.py
@@ -3,17 +3,26 @@ from django.db import models
 class Kunyomi(models.Model):
     kunyomi = models.CharField(max_length=50)
 
+    class Meta:
+        db_table = 'kunyomi'
+
     def __str__(self) -> str:
         return self.kunyomi
 
 class Onyomi(models.Model):
     onyomi = models.CharField(max_length=50)
 
+    class Meta:
+        db_table = 'onyomi'
+
     def __str__(self) -> str:
         return self.onyomi
 
 class Meaning(models.Model):
     meaning = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'meaning'
 
     def __str__(self) -> str:
         return self.meaning
@@ -24,6 +33,9 @@ class Kanji(models.Model):
     kunyomis = models.ManyToManyField(Kunyomi, related_name='kanji_kunyomi')
     onyomis = models.ManyToManyField(Onyomi, related_name='kanji_onyomi')
     meanings = models.ManyToManyField(Meaning, related_name='kanji_meaning')
+
+    class Meta:
+        db_table = 'kanji'
 
     def __str__(self) -> str:
         return self.symbol
