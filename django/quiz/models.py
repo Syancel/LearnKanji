8a2e8f0e9b8699b05984604e5d8fde848cc3c5d6
Marcel Schmitt <schmitt.marcel97@gmail.com>
2022-04-02 10:43:34 +0200

    add str magic method to every model

--- django/quiz/models.py
+++ django/quiz/models.py
@@ -3,11 +3,20 @@ from django.db import models
 class Kunyomi(models.Model):
     kunyomi = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.kunyomi
+
 class Onyomi(models.Model):
     onyomi = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.onyomi
+
 class Meaning(models.Model):
     meaning = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.meaning
 
 class Kanji(models.Model):
     symbol = models.CharField(max_length=10)
@@ -15,3 +24,6 @@ class Kanji(models.Model):
     kunyomis = models.ManyToManyField(Kunyomi, related_name='kanji_kunyomi')
     onyomis = models.ManyToManyField(Onyomi, related_name='kanji_onyomi')
     meanings = models.ManyToManyField(Meaning, related_name='kanji_meaning')
+
+    def __str__(self) -> str:
+        return self.symbol
