dd3c17786609e5e2e24e00cbb577b64100015d06
Marcel Schmitt <schmitt.marcel97@gmail.com>
2022-04-29 12:25:03 +0200

    pull random kanji and display it on the view

--- django/quiz/views.py
+++ django/quiz/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.http import Http404
 
-# Create your views here.
+from .models import Kanji
+
+import random
+
+class QuizView(View):
+    form_class = None
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        kanji_id = random.randint(1, Kanji.objects.all().count())
+        try:
+            kanji = Kanji.objects.get(pk=kanji_id)
+        except Kanji.DoesNotExist:
+            raise Http404("Kanji does not exist")
+        return render(request, self.template_name, 
+                    {'kanji': kanji, 'meanings': None, 'kunyomis': None, 'onyomis': None})
+
+    def post(self, request, *args, **kwargs):
+        pass
