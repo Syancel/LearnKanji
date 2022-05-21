9c699ebceaefdba470e9e8d95c8f80bee51c429a
Marcel Schmitt <schmitt.marcel97@gmail.com>
2022-05-21 20:34:57 +0200

    only use jlpt n5 and n4 kanji and show all possible solutions when answered correctly

--- django/quiz/views.py
+++ django/quiz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db.models import Q
 from django.views import View
 from django.http import Http404
 
@@ -11,14 +12,11 @@ class QuizView(View):
     template_name = 'index.html'
 
     def get(self, request, *args, **kwargs):
-        kanji_id = random.randint(1, Kanji.objects.all().count())
-        try:
-            kanji = Kanji.objects.get(pk=kanji_id)
-        except Kanji.DoesNotExist:
-            raise Http404("Kanji does not exist")
+        easy_kanji = Kanji.objects.filter(Q(jlpt=5)|Q(jlpt=4))
+        random_kanji = random.choice(easy_kanji)
         return render(request, self.template_name, {
-            'kanji_id': kanji_id,
-            'kanji': kanji,
+            'kanji_id': random_kanji.id,
+            'kanji': random_kanji.symbol,
             })
 
     def post(self, request, *args, **kwargs):
@@ -39,6 +37,6 @@ class QuizView(View):
                 break
         
         if is_correct is True:
-            return render(request, 'feedback.html', {'status': True})
+            return render(request, 'feedback.html', {'status': True, 'meanings': meanings})
         else:
             return render(request, 'feedback.html', {'status': False, 'meanings': meanings, 'kanji': kanji})
